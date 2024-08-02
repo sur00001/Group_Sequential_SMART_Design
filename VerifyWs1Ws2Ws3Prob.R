4c50e0a0e47bbc73e5f0b8f3992d673d4160040a
Aparajita Sur <sur00001@umn.edu>
2024-08-02 18:47:50 -0500

    updated

--- VerifyWs1Ws2Ws3Prob.R
+++ VerifyWs1Ws2Ws3Prob.R
@@ -2,9 +2,9 @@
 library(stats)
 
 # Load your data
-load("~/Adaptive SMART paper/Group-Sequential-SMART-Research/Simulated_Zsdata.rda")
+load("C:/Users/lylae/OneDrive/Documents/Adaptive SMART paper/Group-Sequential-SMART-Research/3interim.anal.res.rda")
 
-# Define the necessary variances and covariances
+# Define the necessary variances and covariances (all global variables so make sure to re-run if using other scripts with similar variable names)
 
 VAI_1 <- var(int1.res$muhat.1m1) # it's essentially the same variance for every regime estimate
 VC_1 <- var(int1.res$muhat.0)
@@ -13,28 +13,25 @@ b <- cov(int1.res$muhat.m11, int1.res$muhat.m1m1)
 I1 <- 1 / var(int1.res$zs)
 I2 <- 1 / var(int2.res$zs)
 I3 <- 1 / var(int3.res$zs)
-ws1 = int1.res$ws1 = int1.res$zs*I1
-ws2 = int2.res$ws2 = int2.res$zs*I2
-ws3 = int3.res$ws3 = int3.res$zs*I3
+sim.ws1 = int1.res$ws1 = int1.res$zs*I1
+sim.ws2 = int2.res$ws2 = int2.res$zs*I2
+sim.ws3 = int3.res$ws3 = int3.res$zs*I3
 
 hist(int1.res$zs,main="Interim Analysis 1 zs")
 hist(int2.res$zs,main="Interim Analysis 2 zs")
 hist(int3.res$zs,main="Interim Analysis 3 zs")
-hist(ws1)
-hist(ws2)
-hist(ws3)
+hist(sim.ws1)
+hist(sim.ws2)
+hist(sim.ws3)
 
-theta_s1 = 0
-theta_s2 = 0
-theta_s3 = 0
-
-
+theta_S = 0
+lb = 10
+ub = 60 #infinity takes too long and 60 is above the max ws_j simulated values anyway
 
 
 #-------------------------------------------------------------------------------
 ##Functions to find the marginal density of W_S1
 #-------------------------------------------------------------------------------
-
 
 XS_S_joint_density <- function(x_s,a1,a2,a,b,VAI,VC){
   # Compute f(X_s,S)
@@ -81,17 +78,18 @@ WS1_S_joint_density <- function(w,a1,a2,a,b,VAI,VC,I){
   return(WS_S_joint_density)
 }
 
-# Combined function to compute the marginal density of ws1 over a range
-f_WS1 <- function(ws1_values) {
+# Combined function to compute the marginal density of ws1 over a range lb,ub
+#Using global variables so need to ensure I have the correct components saved in the environment
+f_WS1 <- function(ws1) {
   treatments <- list(c(1, 1), c(1, -1), c(-1, 1), c(-1, -1))
-  joint_dens <- matrix(NA, nrow = length(ws1_values), ncol = length(treatments))
+  joint_dens <- matrix(NA, nrow = length(ws1), ncol = length(treatments))
   
-  for (t in seq_along(treatments)) {
+  for (t in 1:4) {
     treat <- treatments[[t]]
     a1 <- treat[1]
     a2 <- treat[2]
-    for (i in seq_along(ws1_values)) {
-      joint_dens[i, t] <- WS1_S_joint_density(ws1_values[i], a1, a2, a, b, VAI=VAI_1, VC=VC_1, I=I1)
+    for (j in seq_along(ws1)) {
+      joint_dens[j, t] <- WS1_S_joint_density(ws1[j], a1, a2, a, b, VAI=VAI_1, VC=VC_1, I=I1)
     }
   }
   
@@ -99,55 +97,55 @@ f_WS1 <- function(ws1_values) {
   return(density_WS)
 }
 
+#Testing that integrating f(WSI) works and matches the WS1_prob I got in the other R file
+ws1.prob = integrate(f_WS1,lower = 10, upper = ub)$value 
+ws1.prob #it works
+
 #-------------------------------------------------------------------------------
-# Define the density functions to do the recursive integration (using global variables)
+# Define the density functions to do the recursive integration 
 #-------------------------------------------------------------------------------
 f_WS2_given_WS1 <- function(ws2, ws1) {
-  dnorm(ws2, mean = ws1 + theta_S2 * (I2 - I1), sd = sqrt(I2 - I1))
+  dnorm(ws2, mean = ws1 + theta_S * (I2 - I1), sd = sqrt(I2 - I1))
 }
 
 f_WS3_given_WS2 <- function(ws3, ws2) {
-  dnorm(ws3, mean = ws2 + theta_S3 * (I3 - I2), sd = sqrt(I3 - I2))
+  dnorm(ws3, mean = ws2 + theta_S * (I3 - I2), sd = sqrt(I3 - I2))
 }
 
 #-------------------------------------------------------------------------------
 #Recursive integration
 #-------------------------------------------------------------------------------
-#Instead of Infinity, I'm doing 60
+#Instead of Infinity, I'm doing 60 so it won't take so long
 # Integrate f(W_S3 | W_S2) over W_S3
-integrate_WS3 <- function(ws2) {
-  integrate(function(ws3) {
-    f_WS3_given_WS2(ws3, ws2)
-  }, lower = 10, upper = 60)$value
+integrate_WS3 <- function(ws2_values) {
+  integrate(function(ws3_values) {
+    f_WS3_given_WS2(ws3_values, ws2_values)
+  }, lower = lb, upper = ub)$value
 }
 
-integrate_WS3(0)
+#integrate_WS3(10) #check that function runs
 
 # Integrate f(W_S2 | W_S1) * integrate_WS3(ws2) over W_S2
-integrate_WS2 <- function(ws1) {
-  integrate(function(ws2) {
-    f_WS2_given_WS1(ws2, ws1) * integrate_WS3(ws2)
-  }, lower = 10, upper = 60)$value
+integrate_WS2 <- function(ws1_values) {
+  integrate(function(ws2_values) {
+    f_WS2_given_WS1(ws2_values, ws1_values) * integrate_WS3(ws2_values)
+  }, lower = lb, upper = ub)$value
 }
-integrate_WS2(0)
+
+#integrate_WS2(10) #check
 
 # Integrate f(W_S1) * integrate_WS2(ws1) over W_S1
 integrate_WS1 <- function() {
-  integrate(function(ws1) {
-    f_WS1(ws1) * integrate_WS2(ws1)
-  }, lower = 10, upper = 60)$value
+  integrate(function(ws1_values) {
+    f_WS1(ws1_values) * integrate_WS2(ws1_values)
+  }, lower = lb, upper = ub)$value
 }
 
-
 # Compute the probability
-probability <- integrate_WS1()
-
-# Display the result
-print(probability)
+probability <- integrate_WS1(); probability
 
 #Verify with simulated ws1, ws2 and ws3 values
-prop.table(table(ws1>10 & ws2>10 & ws3>10))
-
+prop.table(table(sim.ws1>10 & sim.ws2>10 & sim.ws3>10))
 
 #-------------------------------------------------------------------------------
 # Define the marginal probability function for W_S3 > c
